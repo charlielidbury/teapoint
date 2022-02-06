3c3de8facd776961ea3119145b20eadff46e28aa
Robert Stok <rs.stok@gmail.com>
2022-02-06 00:29:45 +0000

    added templates for matching jobs

--- server/server.py
+++ server/server.py
@@ -1,7 +1,40 @@
-from flask import Flask
+import json
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/home")
 def hello_world():
-    return "<p>Hello, World!</p>"
+  return 
+
+
+@app.route("/find_mutuals")
+def find_mutuals():
+  # read data from DB
+
+  # construct connections graph
+
+  # run mutual finder algorithm for each person
+
+  # update db
+
+  # return
+  return jsonify({
+    "success": True
+  })
+
+
+@app.route("/find_common_interests")
+def find_common_interests():
+  # read data from DB
+
+  # construct connections graph
+
+  # run mutual finder algorithm
+
+  # update db
+
+  # return
+  return jsonify({
+    "success": True
+  })
